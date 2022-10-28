132bcba9412199302d2b1e558ae138f6855566a7
ydu <tyi02038@gmail.com>
2022-10-28 11:25:30 +0900

    bitCoin Checking

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
 import Button from "./Button"
 import styles from "./App.module.css"
 import {useState, useEffect} from "react";
+import { func } from "prop-types";
 
 
-function App() {
-  const [toDo, setToDO] = useState("");
-  const [toDos, setToDOs] = useState([]);
-  const onChange = (event) => setToDO(event.target.value);
-  const onSubmit = (event) => {event.preventDefault();
-  console.log(toDo);
-  if(toDo === ""){
-    return;
-  }
-  setToDO("");
-  setToDOs(currentArray => [toDo, ...currentArray]);
-  console.log(toDos);
-}
- 
-  return (
+function App(){
+  const [loading, setLoading] = useState(true);
+  const [coins, setConins ] = useState([]);
+  const [dollar, setDollar] = useState(0);
+  const [myCoinPrice, setMyCoinPrice] = useState(0);
+
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=10").then(response => response.json())
+    .then(json => {
+      setConins(json)
+      setLoading(false);
+    });
+  }, [])
+    const onDollarChange = (event) => {
+      setDollar(event.target.value);
+    };
+    const onSelect = (event) => {
+      setMyCoinPrice(event.target.value);
+    };
+  return(
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit} >
-      <input value={toDo} onChange={onChange} type="text" placeholder="Write anything" ></input>
-      </form>
-      <button>Add To DO</button>
-    </div>
-  );
+    <h1>coins {loading ? "" : `(${coins.length})`}</h1>
+    {loading ? (
+      <strong>Loading</strong>
+    ) : (
+      <div>
+        <input
+          valuse={dollar}
+          onChange={onDollarChange}
+          type="number"
+          placeholder="price!"
+        />
+        <div>
+
+        <select onChange={onSelect}>
+          {/* <option value={0}>select coin</option> */}
+          {coins.map((coin) => (
+            <option value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+        </div>
+       
+        <input value={dollar/myCoinPrice} type="number" disabled={true} />
+      </div>
+    )}
+  </div>
+);
 }
 
 export default App;
---
+++ src/coinTracker.js
@@ -0,0 +1,34 @@
+import Button from "./Button"
+import styles from "./App.module.css"
+import {useState, useEffect} from "react";
+
+function App() {
+    const [toDo, setToDO] = useState("");
+    const [toDos, setToDOs] = useState([]);
+    const onChange = (event) => setToDO(event.target.value);
+    const onSubmit = (event) => {event.preventDefault();
+    console.log(toDo);
+    if(toDo === ""){
+      return;
+    }
+    setToDO("");
+    setToDOs(currentArray => [toDo, ...currentArray]);
+    console.log(toDos);
+  }
+   
+    return (
+      <div>
+        <h1>My To Dos ({toDos.length})</h1>
+        <form onSubmit={onSubmit} >
+        <input value={toDo} onChange={onChange} type="text" placeholder="Write anything" ></input>
+        <button>Add To DO</button>
+        </form>
+        <hr/>
+        <ul>
+        {toDos.reverse().map((item, index) => <li key={index}>{item}</li>)}
+        </ul>
+      </div>
+    );
+  }
+  
+  export default App;

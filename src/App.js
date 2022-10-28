c59cba890ecce66361b3d0821d4ececa6342ce5a
ydu <tyi02038@gmail.com>
2022-10-28 14:19:02 +0900

    Show Movie

--- src/App.js
+++ src/App.js
@@ -1,58 +1,38 @@
-import Button from "./Button"
-import styles from "./App.module.css"
-import {useState, useEffect} from "react";
-import { func } from "prop-types";
+import { useEffect, useState } from "react";
 
 
-function App(){
-  const [loading, setLoading] = useState(true);
-  const [coins, setConins ] = useState([]);
-  const [dollar, setDollar] = useState(0);
-  const [myCoinPrice, setMyCoinPrice] = useState(0);
 
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=10").then(response => response.json())
-    .then(json => {
-      setConins(json)
+
+function App (){
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async() => {
+     const response = await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      );
+      const json = await response.json();
+      setMovies(json.data.movies);
       setLoading(false);
-    });
-  }, [])
-    const onDollarChange = (event) => {
-      setDollar(event.target.value);
     };
-    const onSelect = (event) => {
-      setMyCoinPrice(event.target.value);
-    };
-  return(
-    <div>
-    <h1>coins {loading ? "" : `(${coins.length})`}</h1>
-    {loading ? (
-      <strong>Loading</strong>
-    ) : (
-      <div>
-        <input
-          valuse={dollar}
-          onChange={onDollarChange}
-          type="number"
-          placeholder="price!"
-        />
-        <div>
-
-        <select onChange={onSelect}>
-          {/* <option value={0}>select coin</option> */}
-          {coins.map((coin) => (
-            <option value={coin.quotes.USD.price}>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
+    useEffect(() => {
+     getMovies()
+    },[])
+    return <div>
+      {loading ? <h1>Loading...</h1> : <div>{movies.map((movie) => (
+      <div key={movie.id}>
+        <img src={movie.medium_cover_image}></img>
+        <h2>{movie.title}</h2>
+        <p>{movie.summary}</p>
+        <ul>
+          {movie.genres.map(g => 
+          <li key={g}>{g}</li> )}
+        </ul>
         </div>
-       
-        <input value={dollar/myCoinPrice} type="number" disabled={true} />
+      ))}
       </div>
-    )}
-  </div>
-);
+      }
+    </div>
+    
 }
 
 export default App;
---
+++ src/movie.js
@@ -0,0 +1,60 @@
+
+
+import Button from "./Button"
+import styles from "./App.module.css"
+import {useState, useEffect} from "react";
+import { func } from "prop-types";
+
+
+function App(){
+  const [loading, setLoading] = useState(true);
+  const [coins, setConins ] = useState([]);
+  const [dollar, setDollar] = useState(0);
+  const [myCoinPrice, setMyCoinPrice] = useState(0);
+
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=10").then(response => response.json())
+    .then(json => {
+      setConins(json)
+      setLoading(false);
+    });
+  }, [])
+    const onDollarChange = (event) => {
+      setDollar(event.target.value);
+    };
+    const onSelect = (event) => {
+      setMyCoinPrice(event.target.value);
+    };
+  return(
+    <div>
+    <h1>coins {loading ? "" : `(${coins.length})`}</h1>
+    {loading ? (
+      <strong>Loading</strong>
+    ) : (
+      <div>
+        <input
+          valuse={dollar}
+          onChange={onDollarChange}
+          type="number"
+          placeholder="price!"
+        />
+        <div>
+
+        <select onChange={onSelect}>
+          {/* <option value={0}>select coin</option> */}
+          {coins.map((coin) => (
+            <option value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+        </div>
+       
+        <input value={dollar/myCoinPrice} type="number" disabled={true} />
+      </div>
+    )}
+  </div>
+);
+}
+
+export default App;
